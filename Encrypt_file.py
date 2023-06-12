3a9eeabf455d2b16d8ba530d5273575090065ae5
Ananya Bhardwaj <97379193+Ananya-Bhardwaj@users.noreply.github.com>
2023-06-12 17:12:30 +0530

    Rename file and added comments

--- Encrypt_file.py
+++ Encrypt_file.py
@@ -2,32 +2,10 @@ from generateKey import keygen
 from cryptography.fernet import Fernet
 from tkinter import filedialog
 
-# with open('filekey.key', 'wb') as filekey:
-#    filekey.write(key)
-
-
-# Open the file that contains the key.
-# Initialize the Fernet object and store it in the fernet variable.
-# Read the original file.
-# Encrypt the file and store it into an object.
-# Then write the encrypted data into the same file nba.csv.
-
-# f = Fernet(key)
-
-# file = open("file.txt", "r+")
-
-# original = file.read()
-
-# encrypted = f.encrypt(bytes(original))
-
-# file.write(encrypted)
-
-# file.close()
-# using the generated key
-
 def Enc(root):
+   # generating a key
    key = keygen(); 
-
+   
    fernet = Fernet(key)
  
    # opening the original file to encrypt
